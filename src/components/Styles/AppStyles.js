b941c462959ef54a883594df2107d7250308696a
Audrey-Willis <nnikky15@gmail.com>
2021-03-25 18:07:53 -0400

    changed color, font, and height of list

--- src/components/Styles/AppStyles.js
+++ src/components/Styles/AppStyles.js
@@ -1,48 +1,49 @@
 import styled from "styled-components";
 
 export const AppStyles = styled.div`
-    font-family: "Permanent Marker", cursive;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  font-family: "Permanent Marker", cursive;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   .fullToDoList {
     display: flex;
     flex-direction: column;
     padding: 2%;
     width: 40%;
+    height:40vh;
     margin-top: 5%;
     margin-bottom: 20%;
-    background-color: #ffff00;
+    background-color: #feff9c;
     background-image: url("https://www.transparenttextures.com/patterns/
-  textured-paper.png");
+    textured-paper.png");
     box-shadow: 15px 15px black;
   }
   p {
     width: 40%;
     text-align: center;
-    font-family: "Varela", sans-serif;
+    font-family: "Montserrat Alternates", sans-serif;
     font-weight: bold;
   }
   @media (max-width: 800px) {
-  .fullToDoList {
-    width: 60%;
-  }
-  p {
-    width: 60%;
-  }
+    .fullToDoList {
+      width: 60%;
+    }
+    p {
+      width: 60%;
+    }
   }
   @media (max-width: 500px) {
-  .fullToDoList {
-    width: 80%;
-  }
-  p {
-    width: 80%;
-  }
-  h1 {
-    font-size: 2rem;
-  }
-  h2 {
-    font-size: 1.2rem;
-  }
+    .fullToDoList {
+      width: 80%;
+    }
+    p {
+      width: 80%;
+    }
+    h1 {
+      font-size: 2rem;
+    }
+    h2 {
+      font-size: 1.2rem;
+    }
   }
 `;
--- src/components/Styles/ItemStyles.js
+++ src/components/Styles/ItemStyles.js
@@ -3,6 +3,5 @@ import styled from "styled-components";
 export const ItemStyles = styled.div`
   margin: 2%;
   font-size: 1.2rem;
-  font-family: "Varela", sans-serif;
-  
+  font-family: "Montserrat Alternates", sans-serif;
 `;
